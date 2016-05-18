7ff87551528951faf7edc8e8351bdf6afe27b756
Jayanth <cjayanth95@gmail.com>
2016-05-18 18:42:57 +0530

    Myfirstcommit

--- Calculator/ViewController.h
+++ Calculator/ViewController.h
@@ -9,6 +9,21 @@
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
+- (IBAction)one:(id)sender;
+- (IBAction)two:(id)sender;
+- (IBAction)three:(id)sender;
+- (IBAction)four:(id)sender;
+- (IBAction)five:(id)sender;
+- (IBAction)six:(id)sender;
+- (IBAction)seven:(id)sender;
+- (IBAction)eight:(id)sender;
+- (IBAction)nine:(id)sender;
+- (IBAction)zero:(id)sender;
+- (IBAction)plus:(id)sender;
+- (IBAction)minus:(id)sender;
+- (IBAction)equals:(id)sender;
+
+
 
 
 @end
--- Calculator/ViewController.m
+++ Calculator/ViewController.m
@@ -24,4 +24,42 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)equals:(id)sender {
+}
+- (IBAction)one:(id)sender {
+}
+- (IBAction)two:(id)sender {
+}
+
+- (IBAction)three:(id)sender {
+}
+
+- (IBAction)four:(id)sender {
+}
+
+- (IBAction)five:(id)sender {
+}
+
+- (IBAction)six:(id)sender {
+}
+
+- (IBAction)seven:(id)sender {
+}
+
+- (IBAction)eight:(id)sender {
+}
+
+- (IBAction)nine:(id)sender {
+}
+
+- (IBAction)zero:(id)sender {
+}
+
+
+- (IBAction)plus:(id)sender {
+}
+
+- (IBAction)minus:(id)sender {
+}
+
 @end
